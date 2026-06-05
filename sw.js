ad0a642ce8b439c910917539508d6200fbb0e092
enginspass-prog <enginspass@gmail.com>
2026-06-05 08:33:06 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-// Unregister this SW
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => {
-  self.registration.unregister();
-  self.clients.matchAll().then(clients => clients.forEach(c => c.navigate(c.url)));
-});
