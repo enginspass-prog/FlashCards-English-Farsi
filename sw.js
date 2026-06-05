3e928239542fd1309378c062af0ef1920739ea2c
enginspass-prog <enginspass@gmail.com>
2026-06-05 08:34:22 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+// Unregister this SW
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+  self.registration.unregister();
+  self.clients.matchAll().then(clients => clients.forEach(c => c.navigate(c.url)));
+});
